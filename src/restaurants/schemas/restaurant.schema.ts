c8e435b5f8abc98eda0d281d48552962ca49c05d
d.khazaei <www.dariushkhazaei629@gmail.com>
2024-02-02 23:47:01 +0330

    handle restaurant schema

---
+++ src/restaurants/schemas/restaurant.schema.ts
@@ -0,0 +1,36 @@
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
+
+export enum Category {
+  FAST_FOOD = 'Fast Food',
+  CAFE = 'Cafe',
+  FINE_DINNING = 'Fine Dinning',
+}
+
+@Schema({
+  timestamps: true,
+})
+export class Restaurant {
+  @Prop()
+  name: string;
+
+  @Prop()
+  description: string;
+
+  @Prop()
+  email: string;
+
+  @Prop()
+  phoneNo: number;
+
+  @Prop()
+  address: string;
+
+  @Prop()
+  category: Category;
+
+  @Prop()
+  images?: object[];
+}
+
+export const RestaurantSchema = SchemaFactory.createForClass(Restaurant);
