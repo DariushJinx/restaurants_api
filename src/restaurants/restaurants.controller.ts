91c53b967a3446f79fffc14960a0595cc0d4f82a
d.khazaei <www.dariushkhazaei629@gmail.com>
2024-02-02 23:45:27 +0330

    handle restaurant controller

---
+++ src/restaurants/restaurants.controller.ts
@@ -0,0 +1,67 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { RestaurantsService } from './restaurants.service';
+import { Restaurant } from './schemas/restaurant.schema';
+import { CreateRestaurantDto } from './dto/create-restaurant.dto';
+import { UpdateRestaurantDto } from './dto/update-restaurant.dto';
+
+@Controller('restaurants')
+export class RestaurantsController {
+  constructor(private restaurantsService: RestaurantsService) {}
+
+  @Get()
+  async getAllRestaurants(): Promise<Restaurant[]> {
+    return this.restaurantsService.findAll();
+  }
+
+  @Post()
+  async createRestaurant(
+    @Body()
+    restaurant: CreateRestaurantDto,
+  ): Promise<Restaurant> {
+    return this.restaurantsService.create(restaurant);
+  }
+
+  @Get(':id')
+  async getRestaurant(
+    @Param('id')
+    id: string,
+  ): Promise<Restaurant> {
+    return this.restaurantsService.findById(id);
+  }
+
+  @Put(':id')
+  async updateRestaurant(
+    @Param('id')
+    id: string,
+    @Body()
+    restaurant: UpdateRestaurantDto,
+  ): Promise<Restaurant> {
+    await this.restaurantsService.findById(id);
+
+    return this.restaurantsService.updateById(id, restaurant);
+  }
+
+  @Delete(':id')
+  async deleteRestaurant(
+    @Param('id')
+    id: string,
+  ): Promise<{ deleted: Boolean }> {
+    await this.restaurantsService.findById(id);
+
+    const restaurant = this.restaurantsService.deleteById(id);
+
+    if (restaurant) {
+      return {
+        deleted: true,
+      };
+    }
+  }
+}
