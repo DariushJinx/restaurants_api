bd21843207c7b88651c8ec1db69d5336e74a7f03
Dariush Khazaei <dariushkhazaei629@gmail.com>
2024-02-04 16:52:19 +0330

    update restaurants module with use PassportModule

--- src/restaurants/restaurants.module.ts
+++ src/restaurants/restaurants.module.ts
@@ -3,14 +3,16 @@ import { RestaurantsController } from './restaurants.controller';
 import { RestaurantsService } from './restaurants.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { RestaurantSchema } from './schemas/restaurant.schema';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
     MongooseModule.forFeature([
       { name: 'Restaurant', schema: RestaurantSchema },
-  ]),
+    ]),
   ],
   controllers: [RestaurantsController],
-  providers: [RestaurantsService]
+  providers: [RestaurantsService],
 })
 export class RestaurantsModule {}
