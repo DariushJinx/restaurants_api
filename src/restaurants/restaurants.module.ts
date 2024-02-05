06457ec83b3a3e3675a282a266d519604d4de242
d.khazaei <www.dariushkhazaei629@gmail.com>
2024-02-06 00:10:06 +0330

    update route about project

--- src/restaurants/restaurants.module.ts
+++ src/restaurants/restaurants.module.ts
@@ -1,9 +1,9 @@
-import { Module } from '@nestjs/common';
 import { RestaurantsController } from './restaurants.controller';
 import { RestaurantsService } from './restaurants.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { RestaurantSchema } from './schemas/restaurant.schema';
 import { PassportModule } from '@nestjs/passport';
+import { Module } from '@nestjs/common/decorators/modules/module.decorator';
 
 @Module({
   imports: [
